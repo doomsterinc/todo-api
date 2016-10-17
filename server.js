a0f208c19b7a369ebb9ce60833a5e96364267edb
doomsterinc <doomsterinc1@gmail.com>
2016-10-17 20:59:36 -0200

    add static data

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: "Meet mom for lunch",
+  complete: false,
+}, {
+  id: 2,
+  description: "Go to market",
+  complete: false,
+}
+];
 
 app.get('/', function(req, res){
   res.send('TODO API Root');

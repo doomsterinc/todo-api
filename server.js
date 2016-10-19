2f04205bc5596d527c4c01c372e9f019a06ef7dd
doomsterinc <doomsterinc1@gmail.com>
2016-10-19 21:29:44 -0200

    cleaning server

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ app.get('/todos', function(req, res){
 app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id);
   var matchedTodo = _.findWhere(todos, {id: todoId});
-// none underscore
-//   var matchedTodo;
-//   todos.forEach(function(todo){
-//     if(todoId === todo.id) {
-//       matchedTodo = todo;
-//     }
-//   });
-//
   if (matchedTodo) {
     res.json(matchedTodo);
   } else {

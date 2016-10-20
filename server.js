e519e67bf6847a8193bb7cefc6274fd9213b92ca
doomsterinc <doomsterinc1@gmail.com>
2016-10-20 00:58:24 -0200

    Add DELETE method in todos

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/todos', function(req, res){
 });
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
-  var todoId = parseInt(req.params.id);
+  var todoId = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoId});
   if (matchedTodo) {
     res.json(matchedTodo);
@@ -41,6 +41,19 @@ app.post('/todos', function(req, res){
   todos.push(body);
 
   res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    res.status(404).json({"error": "ID not found"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
 });
 
 app.listen(PORT, function(){

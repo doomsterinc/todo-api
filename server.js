6875c8058e0f357f62fc9beff364fe92053df220
doomsterinc <doomsterinc1@gmail.com>
2016-10-19 22:02:29 -0200

    Add body data in todos using pick

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos/
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
 
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
     res.status(400).send();

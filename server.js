113b6d4bc9f12addfb1c59c4e791ffeb0e1fac19
doomsterinc <doomsterinc1@gmail.com>
2016-10-21 21:43:38 -0200

    add finally

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.delete('/todos/:id', function(req, res){
 
 //PUT /todos/:id
 app.put('/todos/:id', function(req, res){
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+
   var body = _.pick(req.body, 'description', 'completed');
   var validAttributes = {};
   if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
@@ -66,9 +73,13 @@ app.put('/todos/:id', function(req, res){
     return res.status(400).send();
   }
 
-  if (body.hasOwnProperty('description')) {
-
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+    validAttributes.description = body.description
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
   }
+
+  _.extend(matchedTodo, validAttributes);
 });
 
 app.listen(PORT, function(){

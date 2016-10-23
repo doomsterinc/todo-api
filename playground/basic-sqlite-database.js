676de3e55b8415420d8abc78008228f3b40f9865
doomsterinc <doomsterinc1@gmail.com>
2016-10-23 18:17:06 -0200

    add tests with sequelize 9

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -13,7 +13,7 @@ var Todo = sequelize.define("todo", {
   }
 });
 
-sequelize.sync().then(function() {
+sequelize.sync({force: true}).then(function() {
   console.log('Everything is synced');
 
   Todo.create({

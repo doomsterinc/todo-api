e1357230c6dd01566e85d70e4a20092e43d14fd1
doomsterinc <doomsterinc1@gmail.com>
2016-10-23 17:55:01 -0200

    add tests with sequelize 4

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -3,3 +3,7 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
   'dialect': 'sqlite',
   'storage': 'basic-sqlite-database.sqlite'
 });
+
+sequelize.sync().then(function() {
+  console.log('Everything is synced');
+});

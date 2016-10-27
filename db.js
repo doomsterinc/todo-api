affc9ff45a6a6f5873ec4f2adda543fbe6828e5e
doomsterinc <doomsterinc1@gmail.com>
2016-10-27 11:07:40 -0200

    Correct path of database

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
   'dialect': 'sqlite',
-  'storage': __dirname + '/basic-sqlite-database.sqlite'
+  'storage': __dirname + '/data/dev-todo-api.sqlite'
 });

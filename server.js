1ef76123adde38d88e6c2ac514f9bb05cd450337
doomsterinc <doomsterinc1@gmail.com>
2016-10-28 12:15:48 -0200

    Add corrections in server- Resolved

--- server.js
+++ server.js
@@ -52,10 +52,10 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos', function(req, res){
   var body = _.pick(req.body, 'description', 'completed');
 
-  db.todo.create({body}).then(function(todo){
+  db.todo.create(body).then(function(todo){
     res.json(todo.toJSON());
-;  }, function(e){
-  res.status(400).json(e);
+  }, function(e){
+    res.status(400).json(e);
   });
   // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
   //   res.status(400).send();

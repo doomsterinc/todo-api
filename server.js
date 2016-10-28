d21e1bb8da513d2f501fa80dd493809dbe5832a8
doomsterinc <doomsterinc1@gmail.com>
2016-10-28 13:07:11 -0200

    Add GET method of todo data for sequelize

--- server.js
+++ server.js
@@ -40,12 +40,21 @@ app.get('/todos', function(req, res){
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  if (matchedTodo) {
-    res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
+  db.todo.findById(todoId).then(function(todo){
+    if (!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).send();
+    }
+  }, function(e){
+    res.status(404).json(e);
+  });
+  // var matchedTodo = _.findWhere(todos, {id: todoId});
+  // if (matchedTodo) {
+  //   res.json(matchedTodo);
+  // } else {
+  //   res.status(404).send();
+  // }
 });
 
 // POST /todos/
@@ -57,15 +66,6 @@ app.post('/todos', function(req, res){
   }, function(e){
     res.status(400).json(e);
   });
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-  //   res.status(400).send();
-  // }
-  //
-  // body.description = body.description.trim();
-  // body.id = todoNextID++;
-  // todos.push(body);
-  //
-  // res.json(body);
 });
 
 // DELETE /todos/:id
